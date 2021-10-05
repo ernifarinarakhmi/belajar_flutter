af2bd57c26e99de4cd2eeaaa62939cceae41823b
ernifarinarakhmi <ernifarinarakhmi@gmail.com>
2021-10-05 16:07:42 +0800

    first Commit : Praktik video 3

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Belajar Flutter',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Hello Appbar'),
+        leading: GestureDetector(
+          onTap: () {},
+          child: Icon(Icons.menu),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 15.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: Icon(Icons.search),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 15.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: Icon(Icons.more_vert),
+            ),
+          )
+        ],
+        actionsIconTheme: IconThemeData(color: Colors.black),
+      ),
+    );
+  }
+}
